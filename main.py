9214e57c9d3639f9218e05cb9e638484322c0a90
paulanto09009 <paul@w5y.net>
2018-10-11 07:17:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-from samplealgo import algo
+def initialize(context):  
+    context.spy = sid(21329)  
+    context.order_id = None  
+    context.first_bar = False  
+    context.bar_count = 0  
+    set_slippage(slippage.VolumeShareSlippage(volume_limit=1e-5, price_impact=0))
 
-if __name__ == '__main__':
-    algo.main()
+def handle_data(context, data):  
+    context.bar_count += 1
+
+    if not context.first_bar:  
+        context.order_id = order(context.spy,100)  
+        print context.order_id  
+        context.first_bar = True  
+    print get_order(context.order_id)  
+    print get_order(context.order_id).dt # datetime stamp, order completely filled or cancelled  
+    print get_order(context.order_id).filled # cumulative number of shares filled  
+    print context.portfolio.positions[context.spy]  
+    if context.bar_count == 5:  
+        cancel_order(context.order_id)  
